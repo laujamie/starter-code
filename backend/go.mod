02d41d62ade2832b6871fa47271b5e8d01ab46e1
laujamie <36782341+laujamie@users.noreply.github.com>
2022-09-06 11:04:51 -0400

    Start adding magic to backend

--- backend/go.mod
+++ backend/go.mod
@@ -3,15 +3,23 @@ module github.com/laujamie/starter-code
 go 1.17
 
 require (
+	github.com/btcsuite/btcd v0.0.0-20171128150713-2e60448ffcc6 // indirect
 	github.com/cosmtrek/air v1.40.4 // indirect
 	github.com/creack/pty v1.1.11 // indirect
+	github.com/ethereum/go-ethereum v1.9.24 // indirect
 	github.com/fatih/color v1.10.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/go-resty/resty/v2 v2.3.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/magiclabs/magic-admin-go v0.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/pelletier/go-toml v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	github.com/unrolled/render v1.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"github.com/magiclabs/magic-admin-go"
+	"github.com/magiclabs/magic-admin-go/client"
+	"github.com/magiclabs/magic-admin-go/token"
+
+	rendPkg "github.com/unrolled/render"
 )
 
+const BEARER_SCHEMA = "Bearer"
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := chi.NewRouter()
+	render := rendPkg.New()
+
+	m := client.New("", magic.NewDefaultClient())
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
@@ -26,5 +45,45 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
+
+	r.Post("/api/login", func(w http.ResponseWriter, r *http.Request) {
+		didToken := r.Header.Get("Authorization")
+
+		if !strings.HasPrefix(didToken, BEARER_SCHEMA) {
+			render.JSON(w,
+				http.StatusForbidden,
+				map[string]string{"message": "Bearer token is required"},
+			)
+			return
+		}
+
+		didToken = didToken[len(BEARER_SCHEMA)+1:]
+
+		if didToken == "" {
+			render.JSON(w,
+				http.StatusForbidden,
+				map[string]string{"message": "DID token is required"},
+			)
+			return
+		}
+
+		tk, err := token.NewToken(didToken)
+		if err != nil {
+			render.JSON(w,
+				http.StatusForbidden,
+				map[string]string{
+					"message": fmt.Sprintf("Malformed DID token error: %s", err.Error()),
+				},
+			)
+			return
+		}
+		if err := tk.Validate(); err != nil {
+			render.JSON(w, http.StatusForbidden, map[string]string{"message": err.Error()})
+			return
+		}
+
+		render.JSON(w, http.StatusOK, map[string]string{"message": "Validation successful"})
+	})
+
 	http.ListenAndServe(":5001", r)
 }
