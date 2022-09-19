7c0b8a4daf89efd10db6ec4b727ce5667ead0a79
laujamie <36782341+laujamie@users.noreply.github.com>
2022-09-19 03:44:00 -0400

    Add initial protected route support

--- frontend/src/components/AuthenticatedRoute/index.tsx
+++ frontend/src/components/AuthenticatedRoute/index.tsx
@@ -1,3 +1,21 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import { useAtom } from "jotai";
+import React, { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "~/src/hooks/auth";
+
+type AuthenticatedRouteProps = {
+  children: ReactNode;
+};
+
+export default function AuthenticatedRoute({
+  children,
+}: AuthenticatedRouteProps): ReactNode {
+  const { user, loading: userLoading } = useAuth();
+
+  if (userLoading) return null;
+
+  if (user == null) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
