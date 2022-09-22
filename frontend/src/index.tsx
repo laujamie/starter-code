b466d2cd13fe52b0f30e069694906d69d23dc5a2
laujamie <36782341+laujamie@users.noreply.github.com>
2022-09-22 01:03:30 -0400

    Add initial 404 logic

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "jotai";
 import { useAtomsDebugValue } from "jotai/devtools";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "~/src/App";
 import Home from "~/src/pages/Home";
 import Login from "~/src/pages/Login";
 import Callback from "~/src/pages/Callback";
 import Logout from "~/src/pages/Logout";
+import NotFound from "~/src/pages/NotFound";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -29,7 +30,9 @@ root.render(
             <Route path="login" element={<Login />} />
             <Route path="callback" element={<Callback />} />
             <Route path="logout" element={<Logout />} />
+            <Route path="404" element={<NotFound />} />
           </Route>
+          <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
       </BrowserRouter>
     </Provider>
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "@mantine/core";
+
+export default function NotFound(): JSX.Element {
+  return <Text>404 Not Found</Text>;
+}
